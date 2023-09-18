9192fbcbeb134afc0540383f08374fbbd9639631
AlbertDias <albrtcd@gmail.com>
2023-09-18 11:18:18 -0300

    Atividade de Flask

--- index.py
+++ index.py
@@ -1,10 +1,16 @@
-from flask import Flask
+from flask import Flask,render_template
 
 app = Flask(__name__)
-@app.route('/cardapio')
+@app.route('/')
 def ola():
-    return '<h1> Flask -> </h1>'
+    return render_template('cardapio.html')
 
 @app.route('/pizza')
+def pizza():
+    return render_template('pizza.html')
+
+@app.route("/hamburguer")
+def hamburguer():
+    return render_template('hamburguer.html')
 
 app.run(debug=True)
